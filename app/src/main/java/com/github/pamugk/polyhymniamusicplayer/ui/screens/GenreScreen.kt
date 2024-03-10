fcafcc61f64394e23b2baa32acdd96d5ca32daa2
Alexander Permyakov <pamugk@gmail.com>
2024-03-10 10:40:31 +0500

    UI - implemented proper handling of tracks loading process for the rest of the screens

--- app/src/main/java/com/github/pamugk/polyhymniamusicplayer/ui/screens/GenreScreen.kt
+++ app/src/main/java/com/github/pamugk/polyhymniamusicplayer/ui/screens/GenreScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Warning
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -21,6 +23,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.media3.common.MediaItem
 import androidx.media3.session.MediaBrowser
 import com.github.pamugk.polyhymniamusicplayer.R
+import com.github.pamugk.polyhymniamusicplayer.ui.components.Loader
+import com.github.pamugk.polyhymniamusicplayer.ui.components.StatusPage
+import com.github.pamugk.polyhymniamusicplayer.ui.data.Result
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.guava.await
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,9 +37,15 @@ fun GenreScreen(
     onGoBack: () -> Unit = {},
     padding: PaddingValues = PaddingValues()
 ) {
-    val tracks by produceState(initialValue = emptyList<MediaItem>()) {
-        val response = mediaBrowser.getChildren("genres/$id", 0, 100, null).await()
-        value = response.value ?: emptyList()
+    val result: Result<List<MediaItem>> by produceState(initialValue = Result(state = Result.State.LOADING)) {
+        value = try {
+            val response = mediaBrowser.getChildren("genres/$id", 0, 100, null).await()
+            Result(data = response.value, state = Result.State.SUCCESS)
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Exception) {
+            Result(state = Result.State.ERROR)
+        }
     }
 
     Scaffold(
@@ -52,14 +64,36 @@ fun GenreScreen(
         },
         modifier = Modifier.padding(bottom = padding.calculateBottomPadding())
     ) { innerPadding ->
-        LazyColumn(modifier = Modifier.padding(innerPadding)) {
-            items(tracks) { track ->
-                Text(
-                    text = track.mediaMetadata.title?.toString() ?: stringResource(R.string.no_track),
-                    modifier = Modifier.clickable {
-                        mediaBrowser.setMediaItem(track)
+        when (result.state) {
+            Result.State.ERROR -> StatusPage(
+                description = stringResource(R.string.error_occured),
+                icon = Icons.Default.Error,
+                text = stringResource(R.string.error_occured)
+            )
+            Result.State.LOADING -> Loader(
+                modifier = Modifier.padding(padding),
+                text = stringResource(R.string.tracks_loading)
+            )
+            Result.State.SUCCESS -> {
+                val tracks = result.data
+                if (tracks.isNullOrEmpty()) {
+                    StatusPage(
+                        description = stringResource(R.string.nothing_found),
+                        icon = Icons.Default.Warning,
+                        text = stringResource(R.string.no_tracks_found)
+                    )
+                } else {
+                    LazyColumn(modifier = Modifier.padding(innerPadding)) {
+                        items(tracks) { track ->
+                            Text(
+                                text = track.mediaMetadata.title?.toString() ?: stringResource(R.string.no_track),
+                                modifier = Modifier.clickable {
+                                    mediaBrowser.setMediaItem(track)
+                                }
+                            )
+                        }
                     }
-                )
+                }
             }
         }
     }
